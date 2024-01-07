15069c4669ec4ec0d8941b9edbd56706348c8878
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-07 19:23:32 +0100

    watch works for one

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,5 +1,14 @@
-const showMessage = (text: string) => {
-	console.log(text);
-}
+import express from 'express';
+import * as config from './config';
 
-showMessage('typescript backend works');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.json({
+		appName: "API for AppLearn version 0.1"
+	})
+});
+
+app.listen(config.getPort(), () => {
+	console.log(`AppLearn API is running at http://localhost:${config.getPort()}`);
+});
