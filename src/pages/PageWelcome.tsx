4ca23f769f7dc336cf810a2e9d4433a9d123708c
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-07 19:53:37 +0100

    with flashcards

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,17 @@
+import flashcards from "../../backend/data/flashcards.json";
+
 export const PageWelcome = () => {
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<p>This is the welcome page.</p>
+			<p>There are {flashcards.length} flashcards.</p>
+			<ul className="list-disc ml-6">
+				{flashcards.map(flashcard => {
+					return (
+						<li key={flashcard.suuid}>{flashcard.front}</li>
+					)
+				})}
+			</ul>
+		</>
+	);
+};
